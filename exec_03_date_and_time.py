4420b35d841894bd568154cc8ef8499a12a23637
Bhupesh Kumbhar <bhupesh.kumbhar@gmail.com>
2022-11-28 19:19:38 +0530

    First Commit

---
+++ exec_03_date_and_time.py
@@ -0,0 +1,5 @@
+# Write a Python program to display the current date and time
+import datetime
+now = datetime.datetime.now()
+print ("Current date and time : "+ now.strftime("%Y-%m-%d %H:%M:%S"))
+
